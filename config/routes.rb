77382ce6883a96350265ce902bad7261f5d61a87
Mark Natera <marknatera@gmail.com>
2015-07-01 09:45:46 -0400

    Devise views complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # devise_for :users, path_names: {sign_in: "login", sign_out: "logout", sign_up: "setup"}
-  devise_for :users, :path => '', :path_names => {sign_in: "login", sign_out: "logout", sign_up: "setup", edit: "/account/profile"}
+  devise_for :users, :path => '', :path_names => {sign_in: "login", sign_out: "logout", sign_up: "setup", edit: "/account/profile"}, :controllers => {:registrations => "registrations"}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
