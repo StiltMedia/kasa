68c5225995c40d2d9d10d0b6702fd533992fa147
Mark Natera <marknatera@gmail.com>
2015-07-21 20:52:54 -0400

    Added garage to property

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721225953) do
+ActiveRecord::Schema.define(version: 20150721235743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20150721225953) do
     t.integer  "image_id"
     t.text     "description"
     t.text     "short_description"
+    t.integer  "garage_size"
+    t.boolean  "has_garage"
   end
 
   add_index "properties", ["full_address"], name: "index_properties_on_full_address", unique: true, using: :btree
