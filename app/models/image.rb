bd8562b7ba461ab52d41c221d32849742ea48d0c
Aldo Delgado <aldo@osstek.com>
2015-10-26 23:24:36 -0400

    Added annotations to models

--- app/models/image.rb
+++ app/models/image.rb
@@ -35,3 +35,17 @@ class Image < ActiveRecord::Base
     end
 
 end
+
+# == Schema Information
+#
+# Table name: images
+#
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  property_id        :integer
+#
--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -5,3 +5,13 @@ class FeatureTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: features
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
