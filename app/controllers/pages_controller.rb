7e4857a51d5958989b32f4474f447a28f33fa576
Sam Weerasinghe <oystersauce8@gmail.com>
2015-12-13 11:00:36 -0800

    making listings browse page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def landing
   end
+  def browse
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/landing'
+  get 'pages/browse'
   get 'pages/untitled'
   root 'pages#landing'
 
