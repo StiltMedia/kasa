2726822aeb8a6928b0f6504f4c5dd18bec684d60
banes <djex808@gmail.com>
2015-12-28 17:52:10 +0100

    DEV-16 scrolling issue fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,65 +85,6 @@ $(document).ready(function () {
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-        map.set('styles', [
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "lightness": 100
-                    },
-                    {
-                        "visibility": "simplified"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "color":"#979897"
-                    },
-                    {
-                        "lightness": 50
-                    },
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "color": "#C6E2FF"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#C5E3BF"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#D1D1B8"
-                    }
-                ]
-            }
-        ]);
 
     }
 
