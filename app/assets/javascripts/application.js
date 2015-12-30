92539aa23056d398da1ce1855af91188edf3ebc1
banes <djex808@gmail.com>
2015-12-30 15:48:45 +0100

    Fixing images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -190,6 +190,23 @@ $(document).ready(function () {
         fill: false
     });
 
+    /* listing details carousel*/
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+
 });
 
 function numberWithCommas(x) {
