64263b8131b71e81388e085fd83ff0334612dfb2
Sam Weerasinghe <oystersauce8@gmail.com>
2016-02-19 19:14:54 -0800

    themeing the ticket system

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,7 @@ class Ticket < ActiveRecord::Base
     User.find(Memo.where(ticket_id: self.id).all.order(created_at: :desc).first.mfrom) rescue User.none
   end
 
+  def awaiting_reply(user)
+    self.state != "closed" && user.id != self.last_sayer.id
+  end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "media")
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( style.css custom.js jquery.scrollTo.min.js jquery.sharrre.js bootstrap-slider.js MagnificPopup.js imgLiquid-min.js owl.carousel.min.js components.css plugins.css layout.css default.css custom.css )
+Rails.application.config.assets.precompile += %w( style.css custom.js jquery.scrollTo.min.js jquery.sharrre.js bootstrap-slider.js MagnificPopup.js imgLiquid-min.js owl.carousel.min.js components.css plugins.css layout.css default.css custom.css timeline.css)
 
